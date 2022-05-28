ac5d1acdc796873265838be977c8a9c7b0ab7ade
nfsakmal <mochamad.119140135@student.itera.ac.id>
2022-05-28 18:02:27 +0700

    Update Navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,29 @@
-import "./topbar.scss"
+import "./topbar.scss";
+import {Person, Mail} from "@material-ui/icons";
 
 export default function Topbar() {
   return (
-    <div className="topbar">
+    <div className="topbar active">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">Punten</a>
+          <a href="#intro" className="logo">NapzY</a>
+          <div className="itemContainer">
+            <Person className="icon"/>
+            <span>+62-813-1751-8900</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon"/>
+            <span>mochamad.119140135@gmail.com</span>
+          </div>
         </div>
         <h1>
         </h1>
         <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line1"></span>
+            <span className="line1"></span>
+          </div>
         </div>
       </div>
     </div>
