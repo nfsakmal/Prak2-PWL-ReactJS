49ad9013b8257be474fea1cec9fafc6892febd33
nfsakmal <mochamad.119140135@student.itera.ac.id>
2022-05-28 19:11:36 +0700

    Update navbar + add sidebar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./topbar.scss";
 import {Person, Mail} from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
   return (
-    <div className="topbar active">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">NapzY</a>
@@ -19,7 +19,7 @@ export default function Topbar() {
         <h1>
         </h1>
         <div className="right">
-          <div className="hamburger">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line1"></span>
             <span className="line1"></span>
