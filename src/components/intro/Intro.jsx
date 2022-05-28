b075caca6fd14b44aacf4368df5f586e26e12f75
nfsakmal <mochamad.119140135@student.itera.ac.id>
2022-05-28 20:22:25 +0700

    Intro page (ancur)

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,37 @@
 import './intro.scss'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
-export default function
-() {
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Developer", "unemployed"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
-
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/ganz.png" alt=""/>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hello all, my name is</h2>
+          <h1>Mochamad Nafis Akmalussyifa</h1>
+          <h3>
+            Student <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt=""/>
+        </a>
+      </div>
     </div>
 
   )
